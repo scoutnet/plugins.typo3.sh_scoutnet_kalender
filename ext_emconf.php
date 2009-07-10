fac76036f3981e27d5f992b8f201ae387f600dce
Stefan Horst <stefan@ultrachaos.de>
2009-07-10 08:23:28 +0000

    initial

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "sh_scoutnet_kalender"
+#
+# Auto generated 10-07-2009 10:20
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Scoutnet Calendar',
+	'description' => 'Typo3 plugin to display scoutnet.de calendar',
+	'category' => 'plugin',
+	'author' => 'Stefan Horst',
+	'author_email' => 'stefan.horst@dpsg-koeln.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"4967";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"969d";s:14:"ext_tables.php";s:4:"1fdb";s:13:"locallang.xml";s:4:"ac1f";s:16:"locallang_db.xml";s:4:"42db";s:19:"doc/wizard_form.dat";s:4:"ad5f";s:20:"doc/wizard_form.html";s:4:"b158";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:39:"pi1/class.tx_shscoutnetkalender_pi1.php";s:4:"6a25";s:47:"pi1/class.tx_shscoutnetkalender_pi1_wizicon.php";s:4:"385a";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"d55c";s:24:"pi1/static/editorcfg.txt";s:4:"e688";}',
+);
+
+?>
