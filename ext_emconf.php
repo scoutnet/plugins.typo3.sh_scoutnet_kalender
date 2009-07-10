b75049734c752da572d49a90f96dd0c38e08e46e
Stefan Horst <stefan@ultrachaos.de>
2009-07-10 09:23:53 +0000

    changes

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "sh_scoutnet_kalender"
 #
-# Auto generated 10-07-2009 10:20
+# Auto generated 10-07-2009 11:20
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Stefan Horst',
 	'author_email' => 'stefan.horst@dpsg-koeln.de',
 	'shy' => '',
-	'dependencies' => '',
+	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -32,13 +32,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.0',
 	'constraints' => array(
 		'depends' => array(
+			'cms' => '',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"4967";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"969d";s:14:"ext_tables.php";s:4:"1fdb";s:13:"locallang.xml";s:4:"ac1f";s:16:"locallang_db.xml";s:4:"42db";s:19:"doc/wizard_form.dat";s:4:"ad5f";s:20:"doc/wizard_form.html";s:4:"b158";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:39:"pi1/class.tx_shscoutnetkalender_pi1.php";s:4:"6a25";s:47:"pi1/class.tx_shscoutnetkalender_pi1_wizicon.php";s:4:"385a";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"d55c";s:24:"pi1/static/editorcfg.txt";s:4:"e688";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"4967";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"3ec8";s:17:"ext_localconf.php";s:4:"969d";s:14:"ext_tables.php";s:4:"b542";s:14:"ext_tables.sql";s:4:"2e73";s:13:"locallang.xml";s:4:"ac1f";s:16:"locallang_db.xml";s:4:"1d48";s:19:"doc/wizard_form.dat";s:4:"ba74";s:20:"doc/wizard_form.html";s:4:"a134";s:14:"pi1/ce_wiz.gif";s:4:"40cf";s:39:"pi1/class.tx_shscoutnetkalender_pi1.php";s:4:"6a25";s:47:"pi1/class.tx_shscoutnetkalender_pi1_wizicon.php";s:4:"385a";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"d55c";s:24:"pi1/static/editorcfg.txt";s:4:"e688";}',
 );
 
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,7 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='';
 
 
 t3lib_extMgm::addPlugin(Array('LLL:EXT:sh_scoutnet_kalender/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
@@ -12,4 +13,31 @@ t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","Scoutnet calendar");
 
 
 if (TYPO3_MODE=="BE")	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_shscoutnetkalender_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_shscoutnetkalender_pi1_wizicon.php';
+
+$tempColumns = Array (
+	"tx_shscoutnetkalender_tx_sh_scoutnet_kalender_ids" => Array (		
+		"exclude" => 1,		
+		"label" => "LLL:EXT:sh_scoutnet_kalender/locallang_db.xml:tt_content.tx_shscoutnetkalender_tx_sh_scoutnet_kalender_ids",		
+		"config" => Array (
+			"type" => "input",	
+			"size" => "30",	
+			"checkbox" => "",	
+			"eval" => "required,trim,alphanum,nospace",
+		)
+	),
+	"tx_shscoutnetkalender_tx_sh_scoutnet_kalender_kat_ids" => Array (		
+		"exclude" => 1,		
+		"label" => "LLL:EXT:sh_scoutnet_kalender/locallang_db.xml:tt_content.tx_shscoutnetkalender_tx_sh_scoutnet_kalender_kat_ids",		
+		"config" => Array (
+			"type" => "input",	
+			"size" => "30",	
+			"checkbox" => "",	
+			"eval" => "trim,alphanum,nospace",
+		)
+	),
+);
+
+
+t3lib_div::loadTCA("tt_content");
+t3lib_extMgm::addTCAcolumns("tt_content",$tempColumns,1);
 ?>
