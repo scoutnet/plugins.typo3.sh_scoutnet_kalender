85c283631dadec7bf64df2695204b9a4cc904113
Stefan Horst <stefan@ultrachaos.de>
2009-07-10 09:25:46 +0000

    added fields

--- ext_tables.php
+++ ext_tables.php
@@ -1,19 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='';
-
-
-t3lib_extMgm::addPlugin(Array('LLL:EXT:sh_scoutnet_kalender/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
-
-
-t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","Scoutnet calendar");
-
-
-if (TYPO3_MODE=="BE")	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_shscoutnetkalender_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_shscoutnetkalender_pi1_wizicon.php';
-
 $tempColumns = Array (
 	"tx_shscoutnetkalender_tx_sh_scoutnet_kalender_ids" => Array (		
 		"exclude" => 1,		
@@ -40,4 +27,18 @@ $tempColumns = Array (
 
 t3lib_div::loadTCA("tt_content");
 t3lib_extMgm::addTCAcolumns("tt_content",$tempColumns,1);
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='tx_shscoutnetkalender_tx_sh_scoutnet_kalender_ids,tx_shscoutnetkalender_tx_sh_scoutnet_kalender_kat_ids';
+
+
+t3lib_extMgm::addPlugin(Array('LLL:EXT:sh_scoutnet_kalender/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
+
+
+t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","Scoutnet calendar");
+
+
+if (TYPO3_MODE=="BE")	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_shscoutnetkalender_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_shscoutnetkalender_pi1_wizicon.php';
+
 ?>
