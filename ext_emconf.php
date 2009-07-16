bca21da2ee2b6ade74d7f5925457024a6b0f1e29
Stefan Horst <stefan@ultrachaos.de>
2009-07-16 15:59:54 +0000

    some css foo

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "sh_scoutnet_kalender"
 #
-# Auto generated 16-07-2009 17:47
+# Auto generated 16-07-2009 17:59
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"4967";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"3ec8";s:17:"ext_localconf.php";s:4:"969d";s:14:"ext_tables.php";s:4:"2003";s:14:"ext_tables.sql";s:4:"f3bd";s:13:"locallang.xml";s:4:"ac1f";s:16:"locallang_db.xml";s:4:"dbdb";s:19:"doc/wizard_form.dat";s:4:"22b9";s:20:"doc/wizard_form.html";s:4:"f6c9";s:14:"pi1/ce_wiz.gif";s:4:"40cf";s:39:"pi1/class.tx_shscoutnetkalender_pi1.php";s:4:"338b";s:47:"pi1/class.tx_shscoutnetkalender_pi1_wizicon.php";s:4:"385a";s:13:"pi1/clear.gif";s:4:"cc11";s:21:"pi1/jsonRPCClient.php";s:4:"4e6f";s:16:"pi1/kalender.css";s:4:"977e";s:17:"pi1/locallang.xml";s:4:"d55c";s:17:"pi1/template.html";s:4:"77c6";s:24:"pi1/static/editorcfg.txt";s:4:"e688";}',
+	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"4967";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"3ec8";s:17:"ext_localconf.php";s:4:"969d";s:14:"ext_tables.php";s:4:"2003";s:14:"ext_tables.sql";s:4:"f3bd";s:13:"locallang.xml";s:4:"ac1f";s:16:"locallang_db.xml";s:4:"dbdb";s:19:"doc/wizard_form.dat";s:4:"22b9";s:20:"doc/wizard_form.html";s:4:"f6c9";s:14:"pi1/ce_wiz.gif";s:4:"40cf";s:49:"pi1/class.tx_shscoutnetkalender_jsonRPCClient.php";s:4:"ea3a";s:39:"pi1/class.tx_shscoutnetkalender_pi1.php";s:4:"b6ff";s:47:"pi1/class.tx_shscoutnetkalender_pi1_wizicon.php";s:4:"385a";s:13:"pi1/clear.gif";s:4:"cc11";s:16:"pi1/kalender.css";s:4:"977e";s:17:"pi1/locallang.xml";s:4:"d55c";s:17:"pi1/template.html";s:4:"77c6";s:24:"pi1/static/editorcfg.txt";s:4:"e688";}',
 	'suggests' => array(
 	),
 );
