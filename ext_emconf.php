02475251dc59a67d393be493edc397af99b4d4b2
Stefan Horst <stefan@ultrachaos.de>
2009-09-09 07:43:26 +0000

    beta!!

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "sh_scoutnet_kalender"
 #
-# Auto generated 09-09-2009 09:42
+# Auto generated 09-09-2009 09:43
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"4967";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"3ec8";s:17:"ext_localconf.php";s:4:"969d";s:14:"ext_tables.php";s:4:"2003";s:14:"ext_tables.sql";s:4:"f3bd";s:28:"ext_typoscript_constants.txt";s:4:"9c44";s:24:"ext_typoscript_setup.txt";s:4:"2042";s:13:"locallang.xml";s:4:"ac1f";s:16:"locallang_db.xml";s:4:"dbdb";s:19:"doc/wizard_form.dat";s:4:"22b9";s:20:"doc/wizard_form.html";s:4:"f6c9";s:14:"pi1/ce_wiz.gif";s:4:"40cf";s:39:"pi1/class.tx_shscoutnetkalender_pi1.php";s:4:"53eb";s:47:"pi1/class.tx_shscoutnetkalender_pi1_wizicon.php";s:4:"385a";s:13:"pi1/clear.gif";s:4:"cc11";s:16:"pi1/kalender.css";s:4:"977e";s:17:"pi1/locallang.xml";s:4:"d55c";s:26:"pi1/scoutnet_kalender.tmpl";s:4:"8408";s:24:"pi1/static/editorcfg.txt";s:4:"e688";}',
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"4967";s:10:"README.txt";s:4:"d41d";s:12:"ext_icon.gif";s:4:"3ec8";s:17:"ext_localconf.php";s:4:"969d";s:14:"ext_tables.php";s:4:"2003";s:14:"ext_tables.sql";s:4:"f3bd";s:28:"ext_typoscript_constants.txt";s:4:"9c44";s:24:"ext_typoscript_setup.txt";s:4:"2042";s:13:"locallang.xml";s:4:"ac1f";s:16:"locallang_db.xml";s:4:"dbdb";s:19:"doc/wizard_form.dat";s:4:"22b9";s:20:"doc/wizard_form.html";s:4:"f6c9";s:14:"pi1/ce_wiz.gif";s:4:"40cf";s:39:"pi1/class.tx_shscoutnetkalender_pi1.php";s:4:"53eb";s:47:"pi1/class.tx_shscoutnetkalender_pi1_wizicon.php";s:4:"385a";s:13:"pi1/clear.gif";s:4:"cc11";s:16:"pi1/kalender.css";s:4:"977e";s:17:"pi1/locallang.xml";s:4:"d55c";s:26:"pi1/scoutnet_kalender.tmpl";s:4:"8408";s:24:"pi1/static/editorcfg.txt";s:4:"e688";}',
 	'suggests' => array(
 	),
 );
