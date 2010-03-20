a989e864855d1fc8a85ef5209f39f414ae280e7a
Stefan Horst <stefan@ultrachaos.de>
2010-03-20 16:29:16 +0000

    changed some foo

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "sh_scoutnet_kalender"
+# Extension Manager/Repository config file for ext "sh_scoutnet_kalender".
 #
-# Auto generated 11-11-2009 00:55
+# Auto generated 20-03-2010 17:28
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"4967";s:10:"README.txt";s:4:"d41d";s:12:"ext_icon.gif";s:4:"3ec8";s:17:"ext_localconf.php";s:4:"969d";s:14:"ext_tables.php";s:4:"ffb0";s:14:"ext_tables.sql";s:4:"7b1f";s:28:"ext_typoscript_constants.txt";s:4:"d119";s:24:"ext_typoscript_setup.txt";s:4:"2227";s:13:"locallang.xml";s:4:"ac1f";s:16:"locallang_db.xml";s:4:"27b6";s:19:"doc/wizard_form.dat";s:4:"22b9";s:20:"doc/wizard_form.html";s:4:"f6c9";s:14:"pi1/ce_wiz.gif";s:4:"40cf";s:39:"pi1/class.tx_shscoutnetkalender_pi1.php";s:4:"c3d4";s:47:"pi1/class.tx_shscoutnetkalender_pi1_wizicon.php";s:4:"385a";s:13:"pi1/clear.gif";s:4:"cc11";s:16:"pi1/kalender.css";s:4:"977e";s:17:"pi1/locallang.xml";s:4:"d55c";s:26:"pi1/scoutnet_kalender.tmpl";s:4:"ab9c";s:24:"pi1/static/editorcfg.txt";s:4:"e688";}',
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"4967";s:10:"README.txt";s:4:"d41d";s:12:"ext_icon.gif";s:4:"3ec8";s:17:"ext_localconf.php";s:4:"969d";s:14:"ext_tables.php";s:4:"ffb0";s:14:"ext_tables.sql";s:4:"7b1f";s:28:"ext_typoscript_constants.txt";s:4:"d119";s:24:"ext_typoscript_setup.txt";s:4:"2227";s:13:"locallang.xml";s:4:"415d";s:16:"locallang_db.xml";s:4:"27b6";s:19:"doc/wizard_form.dat";s:4:"22b9";s:20:"doc/wizard_form.html";s:4:"f6c9";s:14:"pi1/ce_wiz.gif";s:4:"40cf";s:39:"pi1/class.tx_shscoutnetkalender_pi1.php";s:4:"c3d4";s:47:"pi1/class.tx_shscoutnetkalender_pi1_wizicon.php";s:4:"8176";s:13:"pi1/clear.gif";s:4:"cc11";s:16:"pi1/kalender.css";s:4:"977e";s:17:"pi1/locallang.xml";s:4:"d55c";s:26:"pi1/scoutnet_kalender.tmpl";s:4:"ab9c";s:24:"pi1/static/editorcfg.txt";s:4:"e688";}',
 	'suggests' => array(
 	),
 );
