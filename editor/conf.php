371e79987c19feaec2371184c17287f374b621c4
Stefan Horst <stefan@ultrachaos.de>
2010-11-04 12:12:43 +0000

    changed lang

--- editor/conf.php
+++ editor/conf.php
@@ -1,6 +1,6 @@
 <?php
 	$MLANG['default']['tabs_images']['tab'] = '../ext_icon.gif';
-	$MLANG['default']['ll_ref']='LLL:EXT:sh_scoutnet_kalender/editor/locallang_mod.xml';
+	$MLANG['default']['ll_ref']='LLL:EXT:sh_scoutnet_kalender/editor/locallang.xml';
 	
 	$MCONF['script']='_DISPATCH';
 	$MCONF['access']='group,user';
