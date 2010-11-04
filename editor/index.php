c3710437d81f8380ad3e379c01a6b5aa0b06ae59
Stefan Horst <stefan@ultrachaos.de>
2010-11-04 17:55:08 +0000

    make language box visible

--- editor/index.php
+++ editor/index.php
@@ -305,6 +305,7 @@ class SC_mod_user_scoutnet_kalender_editor_index extends t3lib_SCbase {
 			$day_options .= '<option value="'.$i.'">'.$i.'</option>'; 
 		}
 
+		$this->createJSFunctions();
 
 		$out .= '</select>'.
 			'<select id="'.$name.'_day">'.$day_options.'</select>'.
