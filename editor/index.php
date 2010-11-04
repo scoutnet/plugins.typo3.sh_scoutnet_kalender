ee3341ab1f1438936abe2e6ecad720be1e922ec0
Stefan Horst <stefan@ultrachaos.de>
2010-11-04 17:14:14 +0000

    make language box visible

--- editor/index.php
+++ editor/index.php
@@ -258,6 +258,99 @@ class SC_mod_user_scoutnet_kalender_editor_index extends t3lib_SCbase {
 		return '<textarea cols="30" rows="8" name="'.$name.'" style="color:'.$color.'" onfocus="if (this.value == \''.$defaultValue.'\') { this.value=\'\'; this.style.color=\'black\';}" onblur="if (this.value ==\'\') {this.style.color=\'lightgray\';this.value=\''.$defaultValue.'\'}">'.$value.'</textarea>'; 
 	}
 
+	private function createDateInput($name, $defaultValue, $value = ""){
+		$color = "black";
+		if ($value == "") {
+			$color = "lightgray";
+			$value = $defaultValue;
+		}
+
+		return '<input type="hidden" name="'.$name.'" id="'.$name.'_value" value="'.$value.'">'.
+'<select name="Start_Date[d]">
+	<option value="1">01</option>
+	<option value="2">02</option>
+	<option value="3">03</option>
+	<option value="4">04</option>
+	<option value="5">05</option>
+	<option value="6">06</option>
+	<option value="7">07</option>
+	<option value="8">08</option>
+	<option value="9">09</option>
+	<option value="10">10</option>
+	<option value="11">11</option>
+	<option value="12">12</option>
+	<option value="13">13</option>
+	<option value="14">14</option>
+	<option value="15">15</option>
+	<option value="16">16</option>
+	<option value="17">17</option>
+	<option value="18">18</option>
+	<option value="19">19</option>
+	<option value="20">20</option>
+	<option value="21">21</option>
+	<option value="22">22</option>
+	<option value="23">23</option>
+	<option value="24">24</option>
+	<option value="25">25</option>
+	<option value="26">26</option>
+	<option value="27">27</option>
+	<option value="28">28</option>
+	<option value="29">29</option>
+	<option value="30" selected="selected">30</option>
+	<option value="31">31</option>
+</select><select name="Start_Date[M]">
+	<option value="1">Jan</option>
+	<option value="2">Feb</option>
+	<option value="3">März</option>
+	<option value="4">April</option>
+	<option value="5">Mai</option>
+	<option value="6">Juni</option>
+	<option value="7">Juli</option>
+	<option value="8">Aug</option>
+	<option value="9">Sept</option>
+	<option value="10">Okt</option>
+	<option value="11" selected="selected">Nov</option>
+	<option value="12">Dez</option>
+</select><select name="Start_Date[Y]">
+	<option value="2009">2009</option>
+	<option value="2010">2010</option>
+	<option value="2011">2011</option>
+	<option value="2012" selected="selected">2012</option>
+	<option value="2013">2013</option>
+	<option value="2014">2014</option>
+	<option value="2015">2015</option>
+	<option value="2016">2016</option>
+	<option value="2017">2017</option>
+	<option value="2018">2018</option>
+	<option value="2019">2019</option>
+	<option value="2020">2020</option>
+	<option value="2021">2021</option>
+	<option value="2022">2022</option>
+	<option value="2023">2023</option>
+	<option value="2024">2024</option>
+	<option value="2025">2025</option>
+	<option value="2026">2026</option>
+	<option value="2027">2027</option>
+	<option value="2028">2028</option>
+	<option value="2029">2029</option>
+	<option value="2030">2030</option>
+</select>';
+
+
+		return '<input maxlength="255" name="'.$name.'" style="color:'.$color.'" type="text" value="'.$value.'" onfocus="if (this.value == \''.$defaultValue.'\') { this.value=\'\'; this.style.color=\'black\';}" onblur="if (this.value ==\'\') {this.style.color=\'lightgray\';this.value=\''.$defaultValue.'\'}">'; 
+	}
+
+	private function createTimeInput($name, $defaultValue, $value = ""){
+		$color = "black";
+		if ($value == "") {
+			$color = "lightgray";
+			$value = $defaultValue;
+		}
+		return '';
+
+		return '<input maxlength="255" name="'.$name.'" style="color:'.$color.'" type="text" value="'.$value.'" onfocus="if (this.value == \''.$defaultValue.'\') { this.value=\'\'; this.style.color=\'black\';}" onblur="if (this.value ==\'\') {this.style.color=\'lightgray\';this.value=\''.$defaultValue.'\'}">'; 
+	}
+
 	/**
 	 * Prints out the module's HTML
 	 *
