beb523363e091b8c7dfb0ad7e225e5990c8a67f7
Stefan Horst <stefan@ultrachaos.de>
2010-11-04 10:32:56 +0000

    make language box visible

--- editor/index.php
+++ editor/index.php
@@ -80,7 +80,7 @@ class SC_mod_user_scoutnet_kalender_editor_index extends t3lib_SCbase {
 		$markers['HEADER1_LABEL'] = "Terminverwaltung";
 		$markers['EBENE_LONG_NAME'] = "Diozese Köln";
 
-		$markers['TERMINE'] = "basbo".$this->doc->moduleTemplate.t3lib_parsehtml::getSubpart($this->doc->moduleTemplate,'###TERMIN###'). "foobaa";
+		$markers['TERMINE'] = t3lib_parsehtml::getSubpart($this->doc->moduleTemplate,'###TERMIN_TEMPLATE###');
 
 
 
