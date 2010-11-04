ffdad8019e9d1609dee7ea49d6a101bbcc8f9ecc
Stefan Horst <stefan@ultrachaos.de>
2010-11-04 18:33:51 +0000

    make language box visible

--- editor/index.php
+++ editor/index.php
@@ -104,11 +104,15 @@ class SC_mod_user_scoutnet_kalender_editor_index extends t3lib_SCbase {
 			$markers['START_DATE_LABEL'] = $GLOBALS['LANG']->getLL('startDateLabel');
 			$markers['START_DATE_MANDATORY'] = $mandatoryAsterisk;
 			$markers['START_DATE_FIELD'] = $this->createDateInput('StartDate',time(),0);
+
 			$markers['START_TIME_LABEL'] = $GLOBALS['LANG']->getLL('startTimeLabel');
+			$markers['START_TIME_FIELD'] = $this->createTimeInput('StartTime',-1,time());
+
 			$markers['END_DATE_LABEL'] = $GLOBALS['LANG']->getLL('endDateLabel');
 			$markers['END_DATE_FIELD'] = $this->createDateInput('EndDate',-1,time());
 
 			$markers['END_TIME_LABEL'] = $GLOBALS['LANG']->getLL('endTimeLabel');
+			$markers['END_TIME_FIELD'] = $this->createTimeInput('StartDate',-1,0);
 
 			$markers['LOCATION_LABEL'] = $GLOBALS['LANG']->getLL('locationLabel');
 			$markers['LOCATION_FIELD'] = $this->createTextInput("Location",$GLOBALS['LANG']->getLL('locationLabel'),""); 
