0dd7f67ecc406157f4bb4f56b7031819d31a61ec
Stefan Horst <stefan@ultrachaos.de>
2010-11-04 18:47:56 +0000

    make language box visible

--- editor/index.php
+++ editor/index.php
@@ -373,8 +373,7 @@ class SC_mod_user_scoutnet_kalender_editor_index extends t3lib_SCbase {
 		}
 
 		for ($min = 0; $min < 60; $min+=5) {
-			$min_options .= '<option value="'.$min.'" '.(!$noContent && strftime("%i",$value)%5 == $min?'selected':'').'>'.$min.'</option>';
-			$out .= strftime("%M",$value)%5;
+			$min_options .= '<option value="'.$min.'" '.(!$noContent && strftime("%M",$value) - strftime("%M",$value)%5 == $min?'selected':'').'>'.$min.'</option>';
 		}
 
 		$out .= '<select id="'.$name.'_hour">'.$hour_options.'</select>'.
