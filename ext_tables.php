7619b23342e704064eb2e48e4ef8aaf130fd533b
Stefan Horst <stefan@ultrachaos.de>
2010-11-05 22:32:35 +0000

    added new be_user field

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,7 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
+// tt_content add fileds
 $tempColumns = Array (
 	"tx_shscoutnetkalender_ids" => Array (		
 		"exclude" => 1,		
@@ -52,14 +53,30 @@ t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='tx_shscoutnetkalender_ids,tx_shscoutnetkalender_optids,tx_shscoutnetkalender_kat_ids,tx_shscoutnetkalender_stufen_ids';
 
+// be_user fileds
+$tempColumns = Array (
+	'tx_shscoutnetkalender_scoutnet_apikey' => Array (		
+		'exclude' => 1,		
+		'label' => 'LLL:EXT:sh_scoutnet_kalender/locallang_db.xml:be_users.tx_shscoutnetkalender_scoutnet_apikey',
+		'config' => Array (
+			'type' => 'input',
+			'size' => '255',
+		)
+	),
+);
 
+
+t3lib_div::loadTCA('be_users');
+t3lib_extMgm::addTCAcolumns('be_users',$tempColumns,1);
+t3lib_extMgm::addToAllTCAtypes('be_users','--div--;LLL:EXT:sh_scoutnet_kalender/locallang_db.xml:be_users.tx_shscoutnetkalender_scounet_tab, tx_shscoutnetkalender_scoutnet_apikey');
+
+
+// add plugins
 t3lib_extMgm::addPlugin(Array('LLL:EXT:sh_scoutnet_kalender/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
-
-
 t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","Scoutnet calendar");
 
 
-
+// add backend user modul
 if (TYPO3_MODE == 'BE') {
 	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_shscoutnetkalender_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_shscoutnetkalender_pi1_wizicon.php';
 
--- ext_tables.sql
+++ ext_tables.sql
@@ -7,3 +7,10 @@ CREATE TABLE tt_content (
 	tx_shscoutnetkalender_kat_ids varchar(255) DEFAULT '' NOT NULL
 	tx_shscoutnetkalender_stufen_ids varchar(255) DEFAULT '' NOT NULL
 );
+
+#
+# Table structure for table 'be_users'
+#
+CREATE TABLE be_users (
+	tx_shscoutnetkalender_scoutnet_api_key varchar(255) DEFAULT '' NOT NULL,
+);
