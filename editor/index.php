240b191b472bc877eb46725fec955e0b56409ef2
Stefan Horst <stefan@ultrachaos.de>
2010-11-05 12:38:18 +0000

    make language box visible

--- editor/index.php
+++ editor/index.php
@@ -181,6 +181,7 @@ class SC_mod_user_scoutnet_kalender_editor_index extends t3lib_SCbase {
 
 			$markers['KEYWORDS_LABEL'] = $GLOBALS['LANG']->getLL('keywordsLabel');
 
+			print_r ($event);
 			foreach ($kalenders[0]['Used_Kategories'] as $id=>$name) {
 				$markers['KEYWORDS_FIELD'] .= '<input name="keywords['.$id.']" type="checkbox" value="1" id="kw_'.$id.'" '.(array_key_exists($id,$event['Kategories'])?'checked':'').'><label for="kw_'.$id.'">'.$name.'</label><br>';
 			}
