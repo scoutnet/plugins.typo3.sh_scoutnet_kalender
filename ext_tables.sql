3d07e47b601562afec2b1cd61121d1003df4a0f4
Stefan Horst <stefan@ultrachaos.de>
2010-11-05 22:37:08 +0000

    create Api Key flag in scoutnet connect

--- ext_tables.sql
+++ ext_tables.sql
@@ -12,5 +12,5 @@ CREATE TABLE tt_content (
 # Table structure for table 'be_users'
 #
 CREATE TABLE be_users (
-	tx_shscoutnetkalender_scoutnet_api_key varchar(255) DEFAULT '' NOT NULL,
+	tx_shscoutnetkalender_scoutnet_apikey varchar(255) DEFAULT '' NOT NULL,
 );
