2279066090197c5ecd3b72ce523691af24c16d6a
Stefan Horst <stefan@ultrachaos.de>
2010-11-05 23:59:44 +0000

    create Api Key flag in scoutnet connect

--- editor/index.php
+++ editor/index.php
@@ -57,7 +57,7 @@ class SC_mod_user_scoutnet_kalender_editor_index extends t3lib_SCbase {
 	public function main() {
 
 		
-		$ids = array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tx_shscoutnetwebservice']['ScoutnetSSID']);
+		$ids = array(intval($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tx_shscoutnetwebservice']['ScoutnetSSID']));
 
 		$docHeaderButtons = $this->getButtons();
 
