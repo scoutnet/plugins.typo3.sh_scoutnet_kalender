446f723826e1256fd44e6ab9acdf94a86b1a44da
Stefan Horst <stefan@ultrachaos.de>
2010-11-05 23:25:01 +0000

    create Api Key flag in scoutnet connect

--- editor/index.php
+++ editor/index.php
@@ -108,7 +108,7 @@ class SC_mod_user_scoutnet_kalender_editor_index extends t3lib_SCbase {
 			
 			$markers['CONTENT'] = $GLOBALS['LANG']->getLL('noApiKeyError');
 			echo "<pre>";
-			print_r($GLOBALS['TYPO3_CONF_VARS']);
+			print_r($GLOBALS['TYPO3_CONF_VARS']['EXT_CONF']['tx_shscoutnetwebservice']);
 			echo "</pre>";
 			die();
 			$markers['SCOUTNET_CONNECT_BUTTON'] = '<form action="https://www.scoutnet.de/community/scoutnetConnect.html" id="scoutnetLogin" method="post" target="_self">
