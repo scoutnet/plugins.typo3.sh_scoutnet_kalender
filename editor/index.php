1598e5130ebfe13e5d4eea93a7a1430b5842ac29
Stefan Horst <stefan@ultrachaos.de>
2010-11-06 21:14:55 +0000

    create Api Key flag in scoutnet connect

--- editor/index.php
+++ editor/index.php
@@ -83,6 +83,7 @@ class SC_mod_user_scoutnet_kalender_editor_index extends t3lib_SCbase {
 			}
 			var checkEmpty = new Array();
 			function checkFields(){
+				alert(foo);
 				for (i=checkEmpty.length -1; i >=0; i--){
 					var field = document.getElementById(checkEmpty[i]);
 					if (field != null) {
@@ -91,6 +92,13 @@ class SC_mod_user_scoutnet_kalender_editor_index extends t3lib_SCbase {
 						}
 					}
 				}
+				return true;
+			}
+			function checkForm(){
+				if (!removeDefaultValues())
+					return false
+				if (!checkFields())
+					return false
 				return true;
 			}
 		";
@@ -240,7 +248,7 @@ die();
 						$subpartMarkers['LAST_MODIFIED_FIELD'] = '';
 					}
 
-					$markers['FORM_HEADER'] = '<form action="'.$this->MCONF['_'].'&action=modify" method="post" name="eventForm" id="eventForm" autocomplete="on" onsubmit="return removeDefaultValues() && checkFields()">';
+					$markers['FORM_HEADER'] = '<form action="'.$this->MCONF['_'].'&action=modify" method="post" name="eventForm" id="eventForm" autocomplete="on" onsubmit="return checkForm()">';
 					$markers['HIDDEN_FIELDS'] = '<input type="hidden" name="mod_snk[event_id]" value="'.$event['ID'].'" />';
 
 					$markers['BACK_TO_OVERVIEW_LINK'] = '<a href="'.$this->MCONF['_'].'">» '.$GLOBALS['LANG']->getLL('backToOverview').'</a>';
