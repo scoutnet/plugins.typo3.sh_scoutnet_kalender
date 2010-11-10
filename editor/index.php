5772bb1a7e6546c0c3b5defef8dd03f2b91f05b8
Stefan Horst <stefan@ultrachaos.de>
2010-11-10 15:11:01 +0000

    changed the output variables

--- editor/index.php
+++ editor/index.php
@@ -165,6 +165,7 @@ class SC_mod_user_scoutnet_kalender_editor_index extends t3lib_SCbase {
 				}
 
 				$rights = $SN->has_write_permission_to_calender(intval($ids),$GLOBALS['BE_USER']->user['tx_shscoutnetkalender_scoutnet_username'],$GLOBALS['BE_USER']->user['tx_shscoutnetkalender_scoutnet_apikey']);
+				echo 'foo';
 
 				if( $rights['code'] != 0) {
 
