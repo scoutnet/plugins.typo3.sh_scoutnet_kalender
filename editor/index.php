52a5ea68e5dcdf8eb7a7e86cce99829cb2ad2f73
Stefan Horst <stefan@ultrachaos.de>
2010-11-10 16:04:29 +0000

    changed the output variables

--- editor/index.php
+++ editor/index.php
@@ -7,6 +7,8 @@ $BE_USER->modAccess($MCONF, 1);
 
 require_once (t3lib_extMgm::extPath('sh_scoutnet_webservice') . 'sn/class.tx_shscoutnetwebservice_sn.php');
 
+require_once (PATH_t3lib.'class.t3lib_scbase.php');
+
 // ***************************
 // Script Classes
 // ***************************
