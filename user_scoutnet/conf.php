49504f3c75e20cb1b169401e36932c6abc55b50b
Stefan Horst <stefan@ultrachaos.de>
2010-11-10 16:24:57 +0000

    renamed folder

---
+++ user_scoutnet/conf.php
@@ -0,0 +1,8 @@
+<?php
+	$MLANG['default']['tabs_images']['tab'] = '../ext_icon.gif';
+	$MLANG['default']['ll_ref']='LLL:EXT:sh_scoutnet_kalender/user_scoutnet/locallang.xml';
+	
+	$MCONF['script']='_DISPATCH';
+	$MCONF['access']='group,user';
+	$MCONF['name']='user_scoutnet';
+?>
