5ea89b8ac2ddc5e5d9f152692f9751e98c988237
Stefan Horst <stefan@ultrachaos.de>
2016-02-19 00:04:23 +0100

    fixed ext_conf

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,39 +6,30 @@
  * Auto generated 03-05-2013 19:13
  *
  * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
+ * Only the data in the array - anything else is removed by next write.
+ * "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'Official Scoutnet Calendar Plugin',
 	'description' => 'Official Typo3 plugin to display scoutnet.de calendar',
 	'category' => 'plugin',
-	'shy' => 0,
-	'version' => '2.0.10',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => 'user_scoutnet',
-	'state' => 'beta',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => 'tt_content, be_user',
-	'clearcacheonload' => 0,
-	'lockType' => '',
-	'author' => 'Stefan "Muetze" Horst',
+	'author' => 'Stefan "Mütze" Horst',
 	'author_email' => 'muetze@scoutnet.de',
-	'author_company' => '',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
-	'constraints' => array (
-		'depends' => array (
-			'typo3' => '4.5.0-7.0.99',
-			'sh_scoutnet_webservice' => '1.0',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 1,
+	'version' => '3.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.0.99',
+			'sh_scoutnet_webservice' => '2.0',
 		),
-		'conflicts' => '',
-		'suggests' => array (
+		'conflicts' => array(
+		),
+		'suggests' => array(
 		),
 	),
 );
-
-?>
