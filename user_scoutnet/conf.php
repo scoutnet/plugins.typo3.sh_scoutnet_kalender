c9e0b20324d6fe0e3cbed886ca65c382b5881ba9
Stefan Horst <stefan@ultrachaos.de>
2016-03-03 19:05:40 +0100

    removed unused files

--- user_scoutnet/conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$MLANG['default']['tabs_images']['tab'] = '../ext_icon.gif';
-	$MLANG['default']['ll_ref']='LLL:EXT:sh_scoutnet_kalender/user_scoutnet/locallang.xml';
-	
-	$MCONF['script']='_DISPATCH';
-	$MCONF['access']='group,user';
-	$MCONF['name']='user_scoutnet';
-?>
